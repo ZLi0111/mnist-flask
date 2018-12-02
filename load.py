d8664af4d741cae0a58ae0c1888983d0a658ad38
johnli-zr <45482633+johnli-zr@users.noreply.github.com>
2018-12-02 15:53:42 +0800

    Add files via upload

---
+++ load.py
@@ -0,0 +1,30 @@
+import numpy as np
+import keras.models
+from scipy.misc import imread, imresize,imshow
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import model_from_json
+
+def init():
+
+    json_file = open('model.json','r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    #load woeights into new model
+    model.load_weights("model.h5")
+    print("Loaded Model from disk")
+    
+    #compile and evaluate loaded model
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    #loss,accuracy = model.evaluate(X_test,y_test)
+    #print('loss:', loss)
+    #print('accuracy:', accuracy)
+    graph = tf.get_default_graph()
+    
+    return model,graph
+
